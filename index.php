db6f4d675cf2a51596deb22d82c7629eff958dd8
LordFB <broekhuizen.f@gmail.com>
2024-04-29 15:07:50 +0200

    Moved nav to the database, created installer, added config

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-// if ( file_exists('./install.php') ){
-//     header('Location: ./install.php');
-//     exit();
-// }
+if ( file_exists('./install.php') ){
+    header('Location: ./install.php');
+    exit();
+}
 
 require_once './inc/util.php';
 
